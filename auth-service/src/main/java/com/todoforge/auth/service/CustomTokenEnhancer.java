eaeef6f1a78e1009006087f3677416c20a98de4d
thaiDev1997 <huynhvanthai.developer@gmail.com>
2024-09-02 10:11:05 +0700

    make refresh_token grant type not return refresh_token value (instead, use original one)

--- auth-service/src/main/java/com/todoforge/auth/service/CustomTokenEnhancer.java
+++ auth-service/src/main/java/com/todoforge/auth/service/CustomTokenEnhancer.java
@@ -1,16 +1,18 @@
 package com.todoforge.auth.service;
 
-import com.todoforge.core.service.RedisService;
-import com.todoforge.resource.dto.AccountPayload;
 import com.todoforge.auth.dto.AccountPermissionDTO;
 import com.todoforge.auth.dto.TodoUser;
 import com.todoforge.core.dto.UserSessionData;
+import com.todoforge.core.service.RedisService;
+import com.todoforge.resource.dto.AccountPayload;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.common.OAuth2RefreshToken;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.OAuth2Request;
 import org.springframework.security.oauth2.provider.token.TokenEnhancer;
 import org.springframework.stereotype.Service;
 
@@ -66,6 +68,15 @@ public class CustomTokenEnhancer implements TokenEnhancer {
 
                 // assign additional data into payload
                 defaultOAuth2AccessToken.setAdditionalInformation(additionalInfo);
+
+                OAuth2Request oAuth2Request = oAuth2Authentication.getOAuth2Request();
+                String grantType = oAuth2Request.getGrantType();
+                boolean isRefreshToken = Objects.isNull(grantType) || "refresh_token".equals(grantType);
+                if (isRefreshToken) {
+                    // grant type is refresh_token -> instead, use the original refresh token
+                    // it doesn't return a new refresh token.
+                    defaultOAuth2AccessToken.setRefreshToken(null);
+                }
             }
         }
         return oAuth2AccessToken;
